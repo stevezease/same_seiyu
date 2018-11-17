1bc9f30ad191b6feedf2c3a4bf69289c6d9b01eb
Steven Jin <stevenjin@Stevens-MBP.fios-router.home>
2018-11-16 19:36:58 -0500

    Polish of initial draft

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Row from './components/row/row'
 import Logo from './res/logo.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faGithubAlt} from '@fortawesome/free-brands-svg-icons'
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons'
+import ReactTooltip from 'react-tooltip'
 
 class App extends Component {
     render() {
@@ -13,6 +15,14 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={Logo} alt="logo"/>
+                    <div data-tip data-for='disclaimer' className="disclaimer-button">
+                            <FontAwesomeIcon icon={faInfoCircle}/>
+                    </div>
+                     <ReactTooltip id='disclaimer' place="left" type="light" effect="solid">
+                         <p>All photos are sourced from MyAnimeList <br/>nand belong to their respective creators.
+                         <br/>Same Seiyu is not endorsed by MyAnimeList <br/>and does not reflect their opinions or views
+                         </p>
+                     </ReactTooltip>
                     <div className="github-button">
                         <a href={'https://github.com/stevezease/same_seiyu'} target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faGithubAlt}/>
@@ -20,7 +30,7 @@ class App extends Component {
                     </div>
                 </header>
                 <div>{
-                    RowData.slice(0, 50).map(rowInfo => <Row key={`row${counter}`} rank={counter++} rowInfo={rowInfo}/>)
+                    RowData.slice(0, 100).map(rowInfo => <Row key={`row${counter}`} rank={counter++} rowInfo={rowInfo}/>)
                 }</div>
             </div>
         );
